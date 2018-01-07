87f47bb6c2e3a254846a376a3267dcea11330c7c
Quentin Gliech <quentingliech@gmail.com>
2018-01-07 22:07:34 +0100

    Use preact and a json file to generate the lines

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
+import { h, render, Component } from "preact";
+/** @jsx h */
+
+import lines from "./lines.json";
 import "./style.styl";
 
-const logo = document.getElementById("logo");
-const button = document.getElementById("animate");
+const Line = ({ index, dashes }) => (
+  <g className={`l${index}`}>
+    {dashes.map(({ color }, j) => (
+      <path
+        d={`m 5 ${index * 20 + 5} l 200 0`}
+        class={["dash", `p${j}`, `c${color}`].join(" ")}
+      />
+    ))}
+  </g>
+);
 
-button.addEventListener("click", () => logo.classList.toggle("initial"));
+const Svg = ({ lines, active }) => (
+  <svg
+    width="210"
+    height="210"
+    viewbox="0 0 210 210"
+    id="logo"
+    class={active ? "" : "initial"}
+    version="1.1"
+  >
+    {lines.map(({ dashes }, index) => <Line dashes={dashes} index={index} />)}
+  </svg>
+);
+
+class App extends Component {
+  toggle() {
+    this.setState({ active: !this.state.active });
+  }
+
+  render() {
+    const toggle = () => this.toggle();
+    return (
+      <div>
+        <Svg active={this.state.active} lines={lines} />
+        <button onClick={toggle}>Animate</button>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.body);
